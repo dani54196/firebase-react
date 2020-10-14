085928e091162b7539fc1b15d493e594d7bf8bb9
Dani54197 <Dani54197@outlook.es>
2020-10-13 21:35:46 -0300

    Updates

---
+++ precache-manifest.4df084359f270cefa1e4de59c3ce41f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bcb96d49d88f7e4743a0c8bfa06ad51",
+    "url": "/firebase-react/index.html"
+  },
+  {
+    "revision": "5de461e992cd6fecc341",
+    "url": "/firebase-react/static/css/2.56d22f2b.chunk.css"
+  },
+  {
+    "revision": "87c9fd75dd7e1a6e35bb",
+    "url": "/firebase-react/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5de461e992cd6fecc341",
+    "url": "/firebase-react/static/js/2.ae0d5343.chunk.js"
+  },
+  {
+    "revision": "baf8e19b81885464d04568dc57ed0d6b",
+    "url": "/firebase-react/static/js/2.ae0d5343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87c9fd75dd7e1a6e35bb",
+    "url": "/firebase-react/static/js/main.5da1d84a.chunk.js"
+  },
+  {
+    "revision": "8e9a97f337c0c3cca910",
+    "url": "/firebase-react/static/js/runtime-main.d08a69c7.js"
+  }
+]);
